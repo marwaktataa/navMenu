65abc27b968295536f3ef7f486cad22e9eda7743
marwaktataa <marwaktata15@gmail.com>
2019-10-17 22:36:53 +0200

    Updates

---
+++ precache-manifest.dcc8e01fee138284e483a2a367fca6e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb0919479ae46e4ba51409e292a842",
+    "url": "/navMenu/index.html"
+  },
+  {
+    "revision": "33505e0084912883661f",
+    "url": "/navMenu/static/css/main.8668c5d1.chunk.css"
+  },
+  {
+    "revision": "5457026030428630505d",
+    "url": "/navMenu/static/js/2.4f2a3216.chunk.js"
+  },
+  {
+    "revision": "33505e0084912883661f",
+    "url": "/navMenu/static/js/main.265d6a80.chunk.js"
+  },
+  {
+    "revision": "a8ab1829e6bb2ad2ae04",
+    "url": "/navMenu/static/js/runtime-main.055d1b2a.js"
+  }
+]);
